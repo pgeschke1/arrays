d7d39dff02e4a6e32c6bf52d13e771c66777b7c5
pgeschke1 <pgeschke@students.mchenry.edu>
2018-02-04 23:20:34 -0600

    Initial commit

---
+++ src/com/paulgeschke/Main.java
@@ -0,0 +1,12 @@
+package com.paulgeschke;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hot Dog Ingredients:");
+        String[] strings = {"Buns", "Hot Dogs", "Ketchup", "Mustard", "Relish"};
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println(strings[i]);
+        }
+    }
+}
